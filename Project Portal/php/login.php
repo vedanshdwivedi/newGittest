983ae117183672dc03aa360441c752fdaf63efe7
Vedansh Dwivedi <vedanshdwivedi0@gmail.com>
2017-09-01 12:43:34 +0530

    Add files via upload

---
+++ Project Portal/php/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Index</title>
+</head>
+<link rel="stylesheet" href="file:///C|/wamp/www/Project Portal/style/style.css" />
+<body background="file:///C|/wamp/www/Project Portal/style/hip-square/hip-square.png"><fieldset>
+<div id="mainimg">
+   <img src="file:///C|/wamp/www/Project Portal/images/cucek logo.jpg" width="122" height="120" align="left" />
+   <h2>&nbsp;&nbsp;Cochin University College of Engineering Kuttanad</h2>
+   <br /><h1>&nbsp;Cochin University of Science and Technology, Kochi-22</h1><br />
+ </div>
+ </fieldset>
+ <fieldset>
+ <ul id="nav">
+   <li><a href="#">Upcoming Events</a></li>
+	<li><a href="#">Academic Calender</a></li>
+	<li><a href="#">Register</a></li>
+	<li><a href="php/login.php">Login</a></li>
+</ul>
+</fieldset>
+<fieldset>
+	<div id="pgname">
+		<h3 align="center">Login Page</h3>
+	</div>
+	<div id="form">
+<form align="center" action="login.php" method="POST">
+	Username : <strong><input type="text" name="username" value="" placeholder="USERNAME"></strong><br /><br />
+	Password : &nbsp;<input type="password" name="password" value="" placeholder="********"><br /><br />
+	<input type="radio" name="Faculty" value="Faculty">&nbsp;Faculty<br />
+	<input type="radio" name="Student" value="Student">&nbsp;Student<br />
+	<input type="radio" name="Class Representative" value="Class Representative">&nbsp;Monitor<br>
+	<input type="submit" name="Submit" value="Submit">
+	&nbsp;&nbsp;&nbsp;&nbsp;
+	<input type="reset" name="reset" value="Reset"><br>
+	<a href="#">Forgot Password?</a><br />
+</form></div>
+</fieldset>
+<fieldset><hr />
+<div id="footer">
+
+ <h4>  Copyright© Designed by Vedansh Dwivedi, CSE CUCEK (2016-2020 batch).</h4>
+</div>
+<hr />
+
+</fieldset>
+</body>
+</html>
